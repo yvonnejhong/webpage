39eb2d7d1431c2fdbe21bdb0039cfec400180c00
Yvonne Hong <yvonnejhong@hotmail.com>
2022-07-08 20:21:25 -0700

    switch logos

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
+from operator import truediv
 from flask import Flask, render_template
+import random
 count = 0
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    name_output = ''
+    a = random.randint(1,100)
+    if a % 2 == 1:
+        name_output = "BABA"
+    else:
+        name_output = "MAMA" 
+
+    b = random.randint(1, 20)
+
+    return render_template('index.html', content=name_output, num=b)
 
 
 @app.route("/test")
@@ -14,4 +25,4 @@ def test():
     return "Count:" + str(count)
     
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
