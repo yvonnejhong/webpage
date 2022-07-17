f9564d30e9376d91899623f00ce6033c40a2c214
Yvonne Hong <yvonnejhong@hotmail.com>
2022-07-17 16:08:36 -0700

    add base template

--- app.py
+++ app.py
@@ -5,24 +5,32 @@ count = 0
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    name_output = ''
-    a = random.randint(1,100)
-    if a % 2 == 1:
-        name_output = "BABA"
-    else:
-        name_output = "MAMA" 
+def index():
+    return render_template('index.html')
 
-    b = random.randint(1, 20)
+@app.route("/games")
+def games():
+    return render_template('games.html')
 
-    return render_template('index.html', content=name_output, num=b)
+@app.route("/writing")
+def writing():
+    return render_template('writing.html')
 
+@app.route("/profile")
+def profile():
+    return render_template('profile.html')
 
-@app.route("/test")
-def test():
-    global count
-    count += 1
-    return "Count:" + str(count)
-    
+@app.route("/ppt")
+def ppt():
+    return render_template('ppt.html')
+
+@app.route("/other")
+def other():
+    return render_template('other.html')
+
+@app.route("/projects")
+def projects():
+    return render_template('projects.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
